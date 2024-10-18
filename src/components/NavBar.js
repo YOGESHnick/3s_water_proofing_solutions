4a07baddadef00b8e01e4fe014ec23b33062b47a
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2024-10-18 09:57:21 +0530

    home setup, navbar done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,14 @@
 import { Link } from "react-router-dom";
+import Logo from "../assets/3sLogo.webp";
 
 export const Navbar = () => {
   return (
     <div>
       <nav className="navbar navbar-expand-lg mnc py-lg-4">
-        <div className="container-fluid f">
+        <div className="container-fluid ">
           <a className="navbar-brand" href="/">
-            {/* <img src="./assets/logo.png" className="logo top-logo" alt="logo"></img> */}
+            <img src={Logo} className="logo top-logo" alt="logo"></img>
+            3S Water Proofing Solutions
           </a>
           <button
             className="navbar-toggler tgl ms-auto"
@@ -19,13 +21,13 @@ export const Navbar = () => {
           >
             <span className="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav ms-auto px-5">
+          <div className="collapse navbar-collapse f" id="navbarNav">
+            <ul className="navbar-nav ms-auto px-5 ">
               <li className="nav-item px-4 gimme-margin-top">
                 <Link className="Link" to="/">Home</Link>
               </li>
               <li className="nav-item px-4 gimme-margin-top">
-                <Link className="Link" to="/bharathanatyam-and-drawing">Services</Link>
+                <Link className="Link" to="/services">Services</Link>
               </li>
               <li className="nav-item px-4 gimme-margin-top">
                 <Link className="Link" to="/gallery">Gallery</Link>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,136 @@
-import React from 'react';
+import React from "react";
+import {
+  Box,
+  Typography,
+  useTheme,
+  useMediaQuery,
+  styled,
+  Grid,
+  Stack,
+} from "@mui/material";
+// import Intro from "../assets/Intro.png";
+// import Hero from "../assets/Hero.png";
+// import Hero2 from "../assets/Hero2.png";
+// import DanceLogo from "../assets/DanceLogo.png";
+// import DrawingLogo from "../assets/DrawingLogo.png";
+
+const Image = styled("img")({
+  maxWidth: "70%",
+  height: "auto",
+  display: "block",
+  margin: "auto",
+  backgroundColor: "transparent",
+});
 
 const Home = () => {
+  const theme = useTheme();
+  const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
   return (
     <div>
-      homens
+
+      {/* W H A T   W E   P R O V I D E */}
+
+      <Box bgcolor="#FFD700" textAlign="center" p={isSmallScreen ? 5 : 10}>
+        <Typography
+          variant="bebasNeue"
+          fontSize={{ md: 48, xs: 30 }}
+          color="black"
+          display="block"
+          mb={5}
+        >
+          WHAT WE PROVIDE?
+        </Typography>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={12} md={6}>
+            <Image
+              // src={DanceLogo}
+              alt="DanceLogo image"
+              style={{
+                maxWidth: "200px",
+                backgroundColor: "transparent",
+              }}
+            />
+            <Typography
+              variant="bebasNeue"
+              fontSize={{ md: 48, xs: 30 }}
+              color="black"
+              display="block"
+              mb={5}
+            >
+              BHARATHANATYAM
+            </Typography>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Image
+              // src={DrawingLogo}
+              alt="DrawingLogo image"
+              style={{
+                maxWidth: "200px",
+                backgroundColor: "transparent",
+              }}
+            />
+            <Typography
+              variant="bebasNeue"
+              fontSize={{ md: 48, xs: 30 }}
+              color="black"
+              display="block"
+              mb={5}
+            >
+              DRAWING
+            </Typography>
+          </Grid>
+        </Grid>
+        <Typography
+          variant="bebasNeue"
+          fontSize={{ md: 48, xs: 30 }}
+          color="black"
+          display="block"
+          // mb={5}
+        >
+          Learn the best of these fine arts
+        </Typography>
+        <Typography
+          variant="bebasNeue"
+          fontSize={{ md: 48, xs: 30 }}
+          color="black"
+          display="block"
+          mb={5}
+        >
+          From the best
+        </Typography>
+        <Box
+          textAlign="center"
+          backgroundColor="transparent"
+          justifyContent="center"
+          px={isSmallScreen ? 1 : 20}
+        >
+          <Typography
+            variant="ptSerif"
+            textAlign="center"
+            fontSize={{ md: 25, xs: 22 }}
+            color="black"
+            sx={{ marginBottom: 2 }}
+            display="block"
+            fontWeight="bold"
+          >
+            Learn the best of the skills, from us at Sri Megala Kalaikudam.
+          </Typography>
+          <Typography
+            variant="ptSerif"
+            textAlign="center"
+            fontSize={{ md: 25, xs: 22 }}
+            color="black"
+            sx={{ marginBottom: 2 }}
+            display="block"
+            fontWeight="bold"
+          >
+            We operate at two centers and different times too! Choose the most
+            appropriate one and join us!
+          </Typography>
+        </Box>
+      </Box>
     </div>
   );
-}
+};
 
 export default Home;
